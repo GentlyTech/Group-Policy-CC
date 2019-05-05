802d24a484e8eec9c5501d18257f27db7b567d14
GentlyTech <andyyep@hotmail.com>
2019-05-05 19:05:50 -0400

    Changed default keybind program from cmd to task manager

--- Group Policy CC/KeybindWizard.Designer.cs
+++ Group Policy CC/KeybindWizard.Designer.cs
@@ -148,7 +148,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(376, 22);
             this.textBox1.TabIndex = 7;
-            this.textBox1.Text = "cmd.exe";
+            this.textBox1.Text = "taskmgr.exe";
             // 
             // button4
             // 
